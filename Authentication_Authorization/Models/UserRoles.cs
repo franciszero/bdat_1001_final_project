4a9c52d00160ff16fc85710570cb8c0568704c78
Francis Chan <franciszero@gmail.com>
2023-07-30 00:32:57 -0400

    .

---
+++ Authentication_Authorization/Models/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Authentication_Authorization.Models
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string Manager = "Manager";
+    }
+}
